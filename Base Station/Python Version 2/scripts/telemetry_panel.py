afc76f893e258c6e747f600d24da9f0b873ceb33
Alex <alexcarrerav2@gmail.com>
2026-05-06 00:33:29 +0200

    re

--- Base Station/Python Version 2/scripts/telemetry_panel.py
+++
@@ -1,9 +0,0 @@
-from nicegui import ui
-
-def createTelemetry():
-
-    vesselCountLabel = ui.label('Vessels: 0')
-    lastUpdateLabel = ui.label('Last update: --')
-    vesselInfoLabel = ui.label('Tracked vessel: ---')
-
-    return vesselCountLabel, lastUpdateLabel, vesselInfoLabel
